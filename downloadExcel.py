537b290a7e2ba12461e73a1138ed250140291b48
Jack Lowry <115683513+Jslowry@users.noreply.github.com>
2023-11-04 13:13:19 -0500

    Excel Path changed
    Now the excel path is set for the actual computer that the program is to be run on.
    -Maddux

--- downloadExcel.py
+++ downloadExcel.py
@@ -3,11 +3,13 @@ import shutil
 
 def updateExcelFile():
     try:
-        filepath = "C:/Users/jacks/Projects/statboardtesting/StatusBoardBulldogMotorsports/Master Schedule 2024.xlsx"
+        filepath = "C:/Users/FSAE/Downloads/StatusBoardTest/StatusBoardBulldogMotorsports-main/Master Schedule 2024.xlsx"
         os.remove(filepath)# removes old first
     except:
         print("No File")
     current_directory = os.getcwd()
     destination_path = os.path.join(current_directory, os.path.basename("Master Schedule 2024.xlsx")) # 
-    # source_file = "C:/Users/jacks/OneDrive - Mississippi State University/Documents - MSSTATE Formula SAE (UserCreated)/Mississippi State FSAE/FSAE 2024/Management/Master Schedule 2024.xlsx"
-    # shutil.copy(source_file, destination_path) # The two lines above are unique to each computer D:
+    source_file = "C:/Users/FSAE/OneDrive - Mississippi State University/Documents - MSSTATE Formula SAE (UserCreated)/Mississippi State FSAE/FSAE 2024/Management/Master Schedule 2024.xlsx"
+    shutil.copy(source_file, destination_path) # The two lines above are unique to each computer D:
+    input()
+    
